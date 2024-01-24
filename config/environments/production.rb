3d7a51542de96fa8bc4aee448096bbf693f2c20a
Ashukla1998 <shuklaashu134@gmail.com>
2024-01-25 00:16:20 +0530

    deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,6 +39,8 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
+  config.action_controller.asset_host = 'https://amitsircoaching.onrender.com/'
+
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
